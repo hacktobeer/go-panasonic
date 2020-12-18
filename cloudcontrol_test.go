2fd5cf4b2f1eab39e871d6f8771d24a2580c42e7
hacktobeer <ramsesdebeer@gmail.com>
2020-12-18 16:50:40 +0100

    Fix control test.

--- cloudcontrol_test.go
+++ cloudcontrol_test.go
@@ -13,7 +13,7 @@ import (
 //var server *httptest.Server
 var (
 	groupsBody  = `{"iaqStatus":{"statusCode":200},"groupCount":1,"groupList":[{"groupId":112867,"groupName":"My House","deviceList":[{"deviceGuid":"CZ-CAPWFC1+B8B7F1B3E326","deviceType":"4","deviceName":"Alaior-home","permission":3,"deviceModuleNumber":"S-125PU2E5B","deviceHashGuid":"f609023332bbeee157a5b868fe80b9fb14a1d883938c1836003796332150db16","summerHouse":0,"iAutoX":false,"nanoe":true,"autoMode":true,"heatMode":true,"fanMode":false,"dryMode":true,"coolMode":true,"ecoNavi":false,"powerfulMode":true,"quietMode":true,"airSwingLR":true,"ecoFunction":0,"temperatureUnit":0,"modeAvlList":{"autoMode":1,"fanMode":1},"autoTempMax":27,"autoTempMin":17,"dryTempMax":30,"dryTempMin":18,"coolTempMax":30,"coolTempMin":18,"heatTempMax":30,"heatTempMin":16,"fanSpeedMode":5,"fanDirectionMode":5,"parameters":{"operate":1,"operationMode":0,"temperatureSet":19.5,"fanSpeed":0,"fanAutoMode":1,"airSwingLR":2,"airSwingUD":3,"ecoMode":0,"ecoNavi":0,"nanoe":1,"iAuto":0,"actualNanoe":1,"airDirection":3,"ecoFunctionData":0}}]}]}`
-	controlBody = `"result":0`
+	controlBody = `{"result":0}`
 )
 
 func serverMock() *httptest.Server {
@@ -60,7 +60,7 @@ func TestTurnOn(t *testing.T) {
 	client.CreateSession("", "", "", server.URL)
 	body, err := client.TurnOn()
 	if err != nil {
-		t.Errorf("TestTurnOn() returned and error: %v", err)
+		t.Errorf("TestTurnOn() returned an error: %v", err)
 	}
 	want := string(pt.SuccessResponse)
 	got := string(body)
